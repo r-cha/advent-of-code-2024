9c1df9ed102de93bcde6df85501c440e36a21e8b
Robert Chandler <robert@akchandler.com>
2024-12-06 15:22:09 -0700

    Day 6 Part 2B

--- day6.py
+++ day6.py
@@ -1,12 +1,17 @@
 import enum
 import sys
+from typing import NamedTuple
 
 
 def read_input():
-    if len(sys.argv) == 2:
+    if len(sys.argv) > 1:
         with open(sys.argv[1]) as f:
             return f.read().strip()
     return sys.stdin.read().strip()
+
+
+def is_indexable(r, c, map):
+    return (0 <= r < len(map)) and (0 <= c < len(map[0]))
 
 
 def safe_get(s, index, default=None):
@@ -21,7 +26,18 @@ class Direction(enum.Enum):
     SOUTH = "v"
     WEST = "<"
 
-    def right(self):
+    def left(self) -> "Direction":
+        match self:
+            case Direction.NORTH:
+                return Direction.WEST
+            case Direction.EAST:
+                return Direction.NORTH
+            case Direction.SOUTH:
+                return Direction.EAST
+            case Direction.WEST:
+                return Direction.SOUTH
+
+    def right(self) -> "Direction":
         match self:
             case Direction.NORTH:
                 return Direction.EAST
@@ -32,42 +48,46 @@ class Direction(enum.Enum):
             case Direction.WEST:
                 return Direction.NORTH
 
+    def offset(self) -> tuple[int, int]:
+        match self:
+            case Direction.NORTH:
+                return (-1, 0)
+            case Direction.EAST:
+                return (0, 1)
+            case Direction.SOUTH:
+                return (1, 0)
+            case Direction.WEST:
+                return (0, -1)
 
-class Guard:
-    def __init__(self, row: int, column: int, direction: Direction) -> None:
-        self.row = row
-        self.column = column
-        self.direction = direction
+
+class GuardBase(NamedTuple):
+    row: int
+    column: int
+    direction: Direction
+
+
+class Guard(GuardBase):
+    __slots__ = ()
+
+    def turn(self):
+        return Guard(self.row, self.column, self.direction.right())
+
+    def next_index(self) -> tuple[int, int]:
+        r, c = self.direction.offset()
+        return self.row + r, self.column + c
 
     def move(self):
-        match self.direction:
-            case Direction.NORTH:
-                self.row -= 1
-            case Direction.EAST:
-                self.column += 1
-            case Direction.SOUTH:
-                self.row += 1
-            case Direction.WEST:
-                self.column -= 1
-        return self
-
-    def ahead_index(self) -> tuple[int, int]:
-        dir_id_map = {
-            Direction.NORTH: (self.row - 1, self.column),
-            Direction.EAST: (self.row, self.column + 1),
-            Direction.SOUTH: (self.row + 1, self.column),
-            Direction.WEST: (self.row, self.column - 1),
-        }
-        return dir_id_map[self.direction]
+        r, c = self.next_index()
+        return Guard(r, c, self.direction)
 
     def look_ahead(self, map: list[str]) -> str | None:
-        row, col = self.ahead_index()
+        row, col = self.next_index()
         return safe_get(safe_get(map, row, ""), col)
 
 
 def parse_map(data: str) -> tuple[list[str], Guard]:
     guard = None
-    map = []
+    map: list[str] = []
     for i, row in enumerate(data.split("\n")):
         r = ""
         for j, col in enumerate(row):
@@ -76,17 +96,18 @@ def parse_map(data: str) -> tuple[list[str], Guard]:
                 guard = Guard(i, j, Direction(col))
         map.append(r)
 
+    assert guard is not None
     return map, guard
 
 
 def traverse_map(map: list[str], guard: Guard) -> set[tuple[int, int]]:
     visited = set()
-    while 0 <= guard.row < len(map) and 0 <= guard.column < len(map[0]):
+    while is_indexable(guard.row, guard.column, map):
         visited.add((guard.row, guard.column))
         ahead_char = guard.look_ahead(map)
         if ahead_char == "#":
-            guard.direction = guard.direction.right()
-        guard.move()
+            guard = guard.turn()
+        guard = guard.move()
     return visited
 
 
@@ -98,58 +119,52 @@ def part1() -> int:
     return len(visited)
 
 
-def add_obstacle(map: list[str], row: int, col: int) -> list[str]:
-    return [
-        "".join("#" if r == row and c == col else map[r][c] for c in range(len(map[r])))
-        for r in range(len(map))
-    ]
-
-
 def debug_print(
     map: list[str],
     guard: Guard,
-    visited: set[tuple[int, int, Direction]],
-    possible_obstacles: set[tuple[int, int]],
-    theoretically_visited: set[tuple[int, int, Direction]] = None,
+    visited: set[Guard],
+    theoretically_visited: set[Guard] | None = None,
+    possible_obstacles: set[tuple[int, int]] | None = None,
 ):
     class Universe(enum.Enum):
         IN_THEORY = 1
         IN_FACT = 2
 
-    def did_visit(row, col, dirs):
+    def did_visit(row: int, col: int, dirs: tuple):
         if theoretically_visited and any(
-            (row, col, d) in theoretically_visited for d in dirs
+            Guard(row, col, d) in theoretically_visited for d in dirs
         ):
             return Universe.IN_THEORY
-        return Universe.IN_FACT if any((row, col, d) in visited for d in dirs) else None
+        return (
+            Universe.IN_FACT
+            if any(Guard(row, col, d) in visited for d in dirs)
+            else None
+        )
 
-    def vertical_visited(row, col):
+    def vertical_visited(row: int, col: int):
         return did_visit(row, col, (Direction.NORTH, Direction.SOUTH))
 
-    def horizontal_visited(row, col):
+    def horizontal_visited(row: int, col: int):
         return did_visit(row, col, (Direction.EAST, Direction.WEST))
 
     for i, row in enumerate(map):
         for j, col in enumerate(row):
-            if (i, j) in possible_obstacles:
-                print("\033[92mO\033[0m", end="")
-            elif (uni1 := horizontal_visited(i, j)) and (
-                uni2 := vertical_visited(i, j)
-            ):
-                if uni1 == Universe.IN_THEORY or uni2 == Universe.IN_THEORY:
-                    print("\033[91m+\033[0m", end="")
+            univ = horizontal_visited(i, j)
+            unih = vertical_visited(i, j)
+            if possible_obstacles and (i, j) in possible_obstacles:
+                print("\033[94mO\033[0m", end="")
+            elif univ or unih:
+                char = "?"
+                if univ and unih:
+                    char = "+"
+                elif univ:
+                    char = "-"
+                elif unih:
+                    char = "|"
+                if univ == Universe.IN_THEORY or unih == Universe.IN_THEORY:
+                    print("\033[91m" + char + "\033[0m", end="")
                 else:
-                    print("+", end="")
-            elif uni := horizontal_visited(i, j):
-                if uni == Universe.IN_THEORY:
-                    print("\033[91m-\033[0m", end="")
-                else:
-                    print("-", end="")
-            elif uni := vertical_visited(i, j):
-                if uni == Universe.IN_THEORY:
-                    print("\033[91m|\033[0m", end="")
-                else:
-                    print("|", end="")
+                    print(char, end="")
             elif i == guard.row and j == guard.column:
                 print(guard.direction.value, end="")
             else:
@@ -158,29 +173,32 @@ def debug_print(
     print()
 
 
-def move_ahead(guard, map, visited):
-    visited.add((guard.row, guard.column, guard.direction))
+def add_obstacle(map: list[str], location: tuple[int, int]) -> list[str]:
+    row, col = location
+    return [
+        "".join("#" if r == row and c == col else map[r][c] for c in range(len(map[r])))
+        for r in range(len(map))
+    ]
+
+
+def move_ahead(guard: Guard, map: list[str], visited: set[Guard]) -> Guard:
+    visited.add(guard)
     ahead_char = guard.look_ahead(map)
-    while ahead_char == "#":
-        guard.direction = guard.direction.right()
-        visited.add((guard.row, guard.column, guard.direction))
-        ahead_char = guard.look_ahead(map)
-    guard.move()
+    if ahead_char == "#":
+        return guard.turn()
+    return guard.move()
 
 
 def check_path_for_loops(
     map: list[str],
     guard: Guard,
-    visited: set[tuple[int, int, Direction]],
+    visited: set[Guard],
 ) -> bool:
-    """Shortcut a full traversal by checking if the guard is in a loop."""
     theoretically_visited = set(visited)
-    while 0 <= guard.row < len(map) and 0 <= guard.column < len(map[0]):
-        if (guard.row, guard.column, guard.direction) in theoretically_visited:
-            debug_print(map, guard, visited, set(), theoretically_visited-visited)
-            input()
+    while is_indexable(guard.row, guard.column, map):
+        if guard in theoretically_visited:
             return True
-        move_ahead(guard, map, theoretically_visited)
+        guard = move_ahead(guard, map, theoretically_visited)
     return False
 
 
@@ -189,33 +207,49 @@ def part2():
     data = read_input()
     map, guard = parse_map(data)
 
-    visited = set()  # with direction
-    possible_obstacles = set()  # Just (row, col) tuples
-    while 0 <= guard.row < len(map) and 0 <= guard.column < len(map[0]):
-        # Imagine a world where there is an obstacle in the way.
-        # If that world loops, then the obstacle is a candidate.
-        if (
-            guard.ahead_index() not in possible_obstacles
-            and check_path_for_loops(
-                add_obstacle(map, *guard.ahead_index()),
-                Guard(guard.row, guard.column, guard.direction.right()),
-                visited,
-            )
+    visited: set[Guard] = set()
+    possible_obstacles: set[tuple[int, int]] = set()
+    while is_indexable(guard.row, guard.column, map):
+        if guard.look_ahead(map) not in {"#", None} and check_path_for_loops(
+            add_obstacle(map, guard.next_index()),
+            Guard(guard.row, guard.column, guard.direction),
+            visited,
         ):
-            possible_obstacles.add(guard.ahead_index())
-        move_ahead(guard, map, visited)
+            possible_obstacles.add(guard.next_index())
+        guard = move_ahead(guard, map, visited)
+    debug_print(map, guard, visited, possible_obstacles=possible_obstacles)
     return len(possible_obstacles)
 
 
-# 1561 too high
-# 1519 too high
-# 1524 will still be too high
-# 1591 way too high
-# 1593 obsiously wrong
-# 1415 is wrong
-# 1378 also wrong (2 hrs later: still wrong)
-# 1614 strikes me as, perhaps, too high
+def err(result: int):
+    attempts = {
+        1614: "strikes me as, perhaps, too high",
+        1593: "obviously wrong",
+        1591: "way too high",
+        1561: "tried this the first time... too high",
+        1524: "will still be too high",
+        1519: "too high",
+        1518: "is the year in which this scenario takes place",
+        1415: "is wrong (still)",
+        1414: "you can't just subtract 1 and expect it to be right",
+        1378: "also wrong",
+        1377: "just as wrong as 1378",
+        1324: "is inexplicably incorrect",
+        1323: "again with the subtracting thing?",
+    }
+    if result == 6:
+        return " \033[1;33m(that's the test data)"
+    if result == 4789:
+        return " \033[1;33mis the answer to part 1!"
+    if result in attempts:
+        return f" \033[91m{attempts[result]}"
+    elif result > 1519:
+        return " \033[91mtoo high"
+    return " \033[0;32mcould be it???"
 
 
 if __name__ == "__main__":
-    print(part2())
+    result = part2()
+    print(result, end="")
+    print(err(result), end="\033[0m")
+    print()
