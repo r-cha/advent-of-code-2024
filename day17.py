49c9f9d3522f61d17f6c8a77c29bfdd5c7883b7c
Robert Chandler <robert@akchandler.com>
2024-12-17 10:31:47 -0700

    Day 17 Part 1 Rewrite

--- day17.py
+++ day17.py
@@ -1,4 +1,5 @@
 import sys
+from enum import Enum
 from typing import NamedTuple
 
 
@@ -28,142 +29,98 @@ Instruction = int
 Program = list[Instruction]
 
 
-class Computer:
-    registers: Registers
-    program: Program
-    instruction_pointer: int
-    stdout: list[int]
-    __slots__ = ["registers", "program", "instruction_pointer", "stdout"]
+class Opcode(Enum):
+    ADV = 0
+    BXL = 1
+    BST = 2
+    JNZ = 3
+    BXC = 4
+    OUT = 5
+    BDV = 6
+    CDV = 7
 
-    def __init__(self, registers, program):
-        self.registers = registers
-        self.program = program
-        self.instruction_pointer = 0
-        self.stdout = []
 
-    def dump_stdout(self):
-        return ",".join(map(str, self.stdout))
+def combo(registers: Registers, operand: int) -> int:
+    match operand:
+        case 0 | 1 | 2 | 3:
+            return operand
+        case 4:
+            return registers.a
+        case 5:
+            return registers.b
+        case 6:
+            return registers.c
+        case _:
+            raise ValueError(f"Invalid combo operand {operand}")
 
-    @property
-    def opcode(self):
-        return self.program[self.instruction_pointer]
 
-    @property
-    def operand(self):
-        return self.program[self.instruction_pointer + 1]
+def _dv(registers: Registers, operand: int) -> int:
+    return registers.a // (2 ** combo(registers, operand))
 
-    @property
-    def combo(self):
-        match self.operand:
-            case 0 | 1 | 2 | 3:
-                return self.operand
-            case 4:
-                return self.registers.a
-            case 5:
-                return self.registers.b
-            case 6:
-                return self.registers.c
-            case _:
-                raise ValueError(f"Invalid combo operand {self.operand}")
 
-    def advance(self):
-        self.instruction_pointer += 2
-        return self
+def adv(registers, operand):
+    return Registers(_dv(registers, operand), registers.b, registers.c)
 
-    def _adv(self):
-        assert self.opcode == 0
-        numerator = self.registers.a
-        denominator = 2**self.combo
-        result = numerator // denominator
-        self.registers = self.registers.write("a", result)
-        return self.advance()
 
-    def _bxl(self):
-        assert self.opcode == 1
-        bitwise_xor = self.registers.b ^ self.operand
-        self.registers = self.registers.write("b", bitwise_xor)
-        return self.advance()
+def bxl(registers, operand):
+    return Registers(registers.a, registers.b ^ operand, registers.c)
 
-    def _bst(self):
-        assert self.opcode == 2
-        result = self.combo % 8
-        self.registers = self.registers.write("b", result)
-        return self.advance()
 
-    def _jnz(self):
-        assert self.opcode == 3
-        if self.registers.a == 0:
-            return self.advance()
-        self.instruction_pointer = self.operand
-        return self
+def bst(registers, operand):
+    return Registers(registers.a, combo(registers, operand) % 8, registers.c)
 
-    def _bxc(self):
-        assert self.opcode == 4
-        bitwise_xor = self.registers.b ^ self.registers.c
-        self.registers = self.registers.write("b", bitwise_xor)
-        return self.advance()
 
-    def _out(self):
-        assert self.opcode == 5
-        self.stdout.append(self.combo % 8)
-        return self.advance()
+def jnz(registers, operand, ip):
+    return ip + 2 if registers.a == 0 else operand
 
-    def _bdv(self):
-        assert self.opcode == 6
-        numerator = self.registers.a
-        denominator = 2**self.combo
-        result = numerator // denominator
-        self.registers = self.registers.write("b", result)
-        return self.advance()
 
-    def _cdv(self):
-        assert self.opcode == 7
-        numerator = self.registers.a
-        denominator = 2**self.combo
-        result = numerator // denominator
-        self.registers = self.registers.write("c", result)
-        return self.advance()
+def bxc(registers, operand):
+    return Registers(registers.a, registers.b ^ registers.c, registers.c)
 
-    def run(self):
-        match self.opcode:
-            case 0:
-                return self._adv()
-            case 1:
-                return self._bxl()
-            case 2:
-                return self._bst()
-            case 3:
-                return self._jnz()
-            case 4:
-                return self._bxc()
-            case 5:
-                return self._out()
-            case 6:
-                return self._bdv()
-            case 7:
-                return self._cdv()
-            case _:
-                raise ValueError(f"Invalid opcode {self.opcode}")
 
-    def run_until_next_output(self):
-        before = len(self.stdout)
-        while not self.halted:
-            self = self.run()
-            if len(self.stdout) > before:
-                return self
-        return self
+def out(registers, operand):
+    return combo(registers, operand) % 8
 
-    @property
-    def halted(self):
-        return self.instruction_pointer >= len(self.program)
 
-    def __str__(self):
-        return (
-            f"Computer(registers={self.registers}, "
-            f"program={self.program}, "
-            f"instruction_pointer={self.instruction_pointer}, "
-            f"stdout={self.dump_stdout()})"
-        )
+def bdv(registers, operand):
+    return Registers(registers.a, _dv(registers, operand), registers.c)
+
+
+def cdv(registers, operand):
+    return Registers(registers.a, registers.b, _dv(registers, operand))
+
+
+operations = {
+    Opcode.ADV: adv,
+    Opcode.BXL: bxl,
+    Opcode.BST: bst,
+    Opcode.BXC: bxc,
+    Opcode.BDV: bdv,
+    Opcode.CDV: cdv,
+}
+
+
+def interpret(r, program):
+    registers = r
+    stdout = []
+    ip = 0
+    while ip < len(program):
+        opcode = Opcode(program[ip])
+        operand = program[ip + 1]
+        # print(ip, opcode, operand, registers, stdout)
+        if op := operations.get(opcode):
+            registers = op(registers, operand)
+            ip += 2
+        elif opcode == Opcode.JNZ:
+            ip = jnz(registers, operand, ip)
+        elif opcode == Opcode.OUT:
+            stdout.append(out(registers, operand))
+            ip += 2
+    return stdout
+
+
+def dump(stdout):
+    return ",".join(map(str, stdout))
 
 
 def parse_data(data):
@@ -179,37 +136,8 @@ def parse_data(data):
 
 def part1():
     registers, program = parse_data(read_input())
-    computer = Computer(registers, program)
-    while computer.instruction_pointer < len(computer.program):
-        computer = computer.run()
-    return computer.dump_stdout()
-
-
-def matches_prefix(prefix, full):
-    return all(a == b for a, b in zip(prefix, full))
-
-
-def part2():
-    _, program = parse_data(read_input())
-    print(program)
-    # We are searching for a quine of program based on Register A.
-    # We'll do this with a computation tree - only run programs that keep generating the right values.
-    # So, advancing through candidate values for A, we'll run each computer until it generates a bad value.
-    # If it never generates a bad value, we've done it!
-    a = 0
-    while True:
-        registers = Registers(a, 0, 0)
-        computer = Computer(registers, program)
-        while computer.instruction_pointer < len(computer.program):
-            computer = computer.run_until_next_output()
-            if not matches_prefix(computer.stdout, program):
-                break
-            else:
-                print("A:", a, "stdout:", computer.dump_stdout())
-                if len(computer.stdout) == len(program):
-                    print(computer.dump_stdout())
-                    return a
-        a += 1
+    stdout = interpret(registers, program)
+    return dump(stdout)
 
 
 if __name__ == "__main__":
