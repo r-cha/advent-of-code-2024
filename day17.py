baec9f04306e0c39e65f6dee10cd2b7bb61fec58
Robert Chandler <robert@akchandler.com>
2024-12-18 11:55:40 -0700

    Day 17 Part 2 - Special Edition

--- day17.py
+++ day17.py
@@ -25,10 +25,6 @@ class Registers(NamedTuple):
                 raise ValueError(f"Invalid register {register}")
 
 
-Instruction = int
-Program = list[Instruction]
-
-
 class Opcode(Enum):
     ADV = 0
     BXL = 1
@@ -40,69 +36,60 @@ class Opcode(Enum):
     CDV = 7
 
 
-def combo(registers: Registers, operand: int) -> int:
+def combo(registers, operand):
     match operand:
         case 0 | 1 | 2 | 3:
-            print(operand)
-            return operand
+            return operand, str(operand)
         case 4:
-            print("a")
-            return registers.a
+            return registers.a, "a"
         case 5:
-            print("b")
-            return registers.b
+            return registers.b, "b"
         case 6:
-            print("c")
             return registers.c
         case _:
             raise ValueError(f"Invalid combo operand {operand}")
 
 
-def _dv(registers: Registers, operand: int) -> int:
-    return registers.a // (2 ** combo(registers, operand))
+def _dv(registers, operand):
+    co, ser = combo(registers, operand)
+    return registers.a // (2 ** co), ser
 
 
 def adv(registers, operand):
-    print("a = a/2^", end="")
-    return Registers(_dv(registers, operand), registers.b, registers.c)
+    res, ser = _dv(registers, operand)
+    return Registers(res, registers.b, registers.c), f"a = a // 2 ** {ser}"
 
 
 def bxl(registers, operand):
-    print(f"b = b ^ {operand}")
-    return Registers(registers.a, registers.b ^ operand, registers.c)
+    return Registers(registers.a, registers.b ^ operand, registers.c), f"b = b ^ {operand}"
 
 
 def bst(registers, operand):
-    print(f"b = ", end="")
-    co = combo(registers, operand)
-    print( "% 8")
-    return Registers(registers.a, combo(registers, operand) % 8, registers.c)
+    co, ser = combo(registers, operand)
+    return Registers(registers.a, co % 8, registers.c), f"b = {ser} % 8"
 
 
 def jnz(registers, operand, ip):
-    print("JNZ")
-    return ip + 2 if registers.a == 0 else operand
+    return ip + 2 if registers.a == 0 else operand, "JNZ"
 
 
 def bxc(registers, operand):
-    print("b = b ^ c")
-    return Registers(registers.a, registers.b ^ registers.c, registers.c)
+    return Registers(registers.a, registers.b ^ registers.c, registers.c), "b = b ^ c"
 
 
 def out(registers, operand):
-    co = combo(registers, operand)
-    print(f"OUT {co} % 8")
-    return co % 8
+    co, ser = combo(registers, operand)
+    return co % 8, f"stdout.append({ser} % 8)"
 
 
 def bdv(registers, operand):
-    print("b = a/2^", end="")
-    return Registers(registers.a, _dv(registers, operand), registers.c)
+    res, ser = _dv(registers, operand)
+    return Registers(registers.a, res, registers.c), f"b = a // 2 ** {ser}"
 
 
 def cdv(registers, operand):
-    print("c = a/2^", end="")
-    return Registers(registers.a, registers.b, _dv(registers, operand))
+    res, ser = _dv(registers, operand)
+    return Registers(registers.a, registers.b, res), f"c = a // 2 ** {ser}"
 
 
 operations = {
@@ -124,12 +111,13 @@ def interpret(r, program):
         operand = program[ip + 1]
         # print(ip, opcode, operand, registers, stdout)
         if op := operations.get(opcode):
-            registers = op(registers, operand)
+            registers, _ = op(registers, operand)
             ip += 2
         elif opcode == Opcode.JNZ:
-            ip = jnz(registers, operand, ip)
+            ip, _ = jnz(registers, operand, ip)
         elif opcode == Opcode.OUT:
-            stdout.append(out(registers, operand))
+            o, _ = out(registers, operand)
+            stdout.append(o)
             ip += 2
     return stdout
 
@@ -155,15 +143,7 @@ def part1():
     return dump(stdout)
 
 
-def simplified_test_for_a(a):
-    stdout = []
-    while a:
-        a = a >> 3 # a // 8
-        res.append(a % 8)
-    return res
-
-
-def simplified_for_a(a):
+def manual_for_a(a):
     """
     For my program,
     > 2,4,1,5,7,5,1,6,4,1,5,5,0,3,3,0
@@ -184,18 +164,60 @@ def simplified_for_a(a):
     return stdout
 
 
+def transpile(registers, program):
+    stdout = []
+    ip = 0
+    pstr = []
+    while ip < len(program):
+        opcode = Opcode(program[ip])
+        operand = program[ip + 1]
+        if op := operations.get(opcode):
+            registers, loc = op(registers, operand)
+            ip += 2
+        elif opcode == Opcode.JNZ:
+            ip, loc = jnz(registers, operand, ip)
+            return pstr
+        elif opcode == Opcode.OUT:
+            o, loc = out(registers, operand)
+            stdout.append(o)
+            ip += 2
+        pstr.append(loc)
+    return pstr
+
+
+def simplified_for_a(pstr):
+    """
+    Given a simplified program, return a function that computes it for any value of A.
+    """
+    function = """
+def p(a):
+    b = c = 0
+    stdout = []
+    while a:
+"""
+    for line in pstr:
+        function += f"        {line}\n"
+
+    function += "    return stdout\n"
+    print(function)
+    exec(function, globals())
+    return globals()["p"]
+
+
 def part2():
     """Find a value for A such that program p is a Quine, i.e. p(a) == p()"""
-    _, p = parse_data(read_input())
+    registers, program = parse_data(read_input())
+    python_program = simplified_for_a(transpile(registers, program))
+
     candidates = {0}
     # Starting from the last instruction (since it's the easiest),
     # Work backward with greater and greater A values until we have the coplete program.
-    for instruction in reversed(p):
+    for instruction in reversed(program):
         new_candidates = set()
         for a in candidates:
             shifted = a * 8
             for candidate in range(shifted, shifted+8):
-                stdout = simplified_for_a(candidate)
+                stdout = python_program(candidate)
                 if stdout and stdout[0] == instruction:
                     new_candidates.add(candidate)
         candidates = new_candidates
